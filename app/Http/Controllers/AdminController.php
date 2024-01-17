287cea200b4f8335219bc12be4aa337b35154f85
NowaT5 <nowa2536@gmail.com>
2024-01-17 17:59:10 +0700

    now- make controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    function index(){ #แสดงผลหน้าแรก 
+
+    }
+    function list_product(){ #แสดงรายการสินค้า
+
+    }
+    function make_order(){ #สร้างคำสั่งซื้อ
+
+    }
+    function shipment(){ #สถานะการจัดส่ง
+
+    }
+}
